69a31c1f15531e865fadaf0a94ebe1519a5ca8c9
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-09-20 20:57:59 -0300

    Adding toast to login layout

--- resources/js/layouts/LoginLayout/index.tsx
+++ resources/js/layouts/LoginLayout/index.tsx
@@ -1,3 +1,4 @@
+import { AppToast } from "@/components/AppToast"
 import { ReactNode } from "react"
 
 interface LoginLayoutProps {
@@ -6,11 +7,15 @@ interface LoginLayoutProps {
 
 const LoginLayout = ({children}: LoginLayoutProps) => {
     return (
-        <main className="h-screen flex items-center justify-center">
-            <div className="container mx-auto max-w-xl">
-                {children}
-            </div>
-        </main>
+        <>
+            <main className="h-screen flex items-center justify-center">
+                <div className="container mx-auto max-w-xl">
+                    {children}
+                </div>
+            </main>
+
+            <AppToast />
+        </>
     )
 }
 
