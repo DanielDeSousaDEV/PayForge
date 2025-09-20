1f264c33d0e6cf91a26177d1ee03030a7d500810
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-09-20 20:58:46 -0300

    adding new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Auth;
@@ -10,19 +11,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+    
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.store');
+});
 
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
     Route::get('/product/{id}', [ProductController::class, 'show'])->name('product');
-    
-    Route::inertia('/profile', 'Profile');
+
+    Route::get('/cart', [CartController::class, 'show'])->name('cart');
+    Route::post('/cart/{id}/add', [CartController::class, 'addProduct'])->name('cart.add');
+
+    Route::get('/profile', [AuthController::class, 'showProfile'])->name('profile');
+    Route::post('/profile', [AuthController::class, 'updateProfile'])->name('profile.update');
 });
+
+
+
+
+
 
 Route::get('/log', fn () => Auth::loginUsingId(5));
 Route::get('/logout', fn () => Auth::logout());
