5c1b38184526edd17c0a31f6a498a2fb78b471cf
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-09-20 20:56:00 -0300

    adding toast to home layout

--- resources/js/layouts/HomeLayout/index.tsx
+++ resources/js/layouts/HomeLayout/index.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
 import { Link } from "@inertiajs/react";
+import { Footer } from "@/components/Footer/Index";
+import { AppToast } from "@/components/AppToast";
 
 interface HomeLayoutProps {
     children: ReactNode
@@ -13,42 +15,9 @@ export default function HomeLayout({ children }: HomeLayoutProps) {
 
             {children}
 
-            <footer className="bg-tertiary">
-                <div className="container mx-auto grid grid-cols-4 py-4 px-2">
-                    <div className="col-span-2">
-                        <h4 className="text-2xl font-semibold font-heading text-gray-100 mb-2">PayForge</h4>   
-                        <p className="text-sm text-gray-100">Produzido por {' '}
-                            <a className="text-blue-400 underline visited:text-purple-400" href="https://github.com/DanielDeSousaDEV">
-                                Daniel De Sousa
-                            </a>
-                        </p>
-                    </div>
+            <Footer/>
 
-                    <div >
-                        <div className="flex flex-col leading-relaxed">
-                            <Link href="/" className="text-gray-100 visited:text-gray-300">
-                                Home
-                            </Link>
-                            <Link href="/profile" className="text-gray-100 visited:text-gray-300">
-                                Profile
-                            </Link>
-                            <Link href="/Cart" className="text-gray-100 visited:text-gray-300">
-                                Cart
-                            </Link>
-                        </div>
-                    </div>
-                    <div >
-                        <div className="flex flex-col leading-relaxed">
-                            <a href="https://github.com/DanielDeSousaDEV" className="text-gray-100 visited:text-gray-300">
-                                Github
-                            </a>
-                            <a href="https://www.linkedin.com/in/daniel-de-sousa-257275314/" className="text-gray-100 visited:text-gray-300">
-                                LinkedIn
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            <AppToast />
         </main>
     )
 }
