dc855f8660c26a31d5ec84ac0375bb17a28c1879
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-09-27 20:47:41 -0300

    feat: adding routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/products', [ProductController::class, 'showProductsPanel'])->name('admin.products');
     Route::post('/admin/products', [ProductController::class, 'storeProduct'])->name('admin.products.store');
     Route::delete('/admin/products/{id}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
+
+    Route::get('/admin/users', [UserController::class, 'showUsersPanel'])->name('admin.users');
+    Route::post('/admin/users', [UserController::class, 'storeUser'])->name('admin.users.store');
+    Route::delete('/admin/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
 });
 
 
