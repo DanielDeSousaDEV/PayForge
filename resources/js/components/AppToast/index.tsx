eb49c6d89cdb83aa8a71425d986f02d65a5d4083
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-09-28 20:21:06 -0300

    chore: adding more time for flash messages

--- resources/js/components/AppToast/index.tsx
+++ resources/js/components/AppToast/index.tsx
@@ -20,7 +20,7 @@ export function AppToast() {
                 setAnimate("out")
                 // espera a animação terminar antes de desmontar
                 setTimeout(() => setVisible(false), 300)
-            }, 5000)
+            }, 7000)
         }
 
         return () => {
